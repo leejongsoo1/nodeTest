0c33bda2db8461ea7ee5bb7916296a2776e9317f
leejongsoo1 <jongsoo996@gmail.com>
2020-11-26 22:51:07 +0900

    1126

--- project/routes/mail.js
+++ project/routes/mail.js
@@ -2,19 +2,16 @@ var express = require('express');
 var mail = require('../service/mailService');
 var router = express.Router();
 
-var dice = 0;
-
 router.post('/send', (req, res, next) => {
     let email = req.body.email;
-    dice = mail(email, 'password');
-    res.json({status: "OK"});
+    mail(email, res);    
 });
 
 router.post('/checkCode', (req, res, next) => {
     let code = req.body.code;
-    console.log(dice);
-    if(dice == code) {
-        dice = 0;
+    console.log(req.session.emailCode);
+    if(req.session.emailCode == code) {
+        req.session.emailCode = 0;
         res.json({status: "OK"});
     } else {
         res.json({status: "False"});
--- project/service/mailService.js
+++ project/service/mailService.js
@@ -1,41 +1,33 @@
 var mailer = require('nodemailer');
 
-let transport = mailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'jongsoo996@gmail.com',
-        pass: 'bfmvhjriemgjhczv'
-    }
-});
+module.exports = (email, res) => {
+    
+    var getRandomInt = (min, max) => {
+        return Math.floor(Math.random() * (max - min)) + min;
+    };
 
-var getRandomInt = (min, max) => {
-    return Math.floor(Math.random() * (max - min)) + min;
-};
-
-module.exports = (email, purpose) => {
-
-    var subject = '';
-    var text = '';
+    let transport = mailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'jongsoo996@gmail.com',
+            pass: 'bfmvhjriemgjhczv'
+        }
+    });
 
     var dice = getRandomInt(0, 999999);
-
-    if(purpose == 'password') {
-        subject = 'Node 회원 인증 이메일 입니다.';
-        text = `귀하의 인증번호는 ${dice} 입니다. \n받으신 인증번호를 입력하시면 다음으로 넘어갑니다.`;
-    }
 
     let mailOptions = {
         from: 'jongsoo996@gmail.com',
         to: email,
-        subject: subject,
-        text: text
+        subject: 'Node 회원 인증 이메일 입니다.',
+        text: `귀하의 인증번호는 ${dice} 입니다. \n받으신 인증번호를 입력하시면 다음으로 넘어갑니다.`
     }
 
     transport.sendMail(mailOptions, (err, info) => {
         if(err) console.log(err);
         else {
             console.log('send success!! ' + info.response);
-            return dice;
+            res.json({status: "OK"});            
         }
     });
     
